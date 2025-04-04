57ad7c0ffa4a75f4e9b2f808c3214b817fe2d522
Juperaz1 <loiseaujustine6@gmail.com>
2025-04-04 14:18:07 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/ppe1/MainActivity.java
@@ -0,0 +1,36 @@
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText etFirstName = findViewById(R.id.etFirstName);
+        EditText etLastName = findViewById(R.id.etLastName);
+        EditText etPassword = findViewById(R.id.etPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = etFirstName.getText().toString().trim();
+                String lastName = etLastName.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                if (firstName.isEmpty() || lastName.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Connexion en cours", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        });
+    }
+}
